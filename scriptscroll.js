322f8d44b55aafdb4b9ca0c1fc39bcda8572e954
ver-eng <v.patsinashvili@gmail.com>
2024-07-28 21:41:12 +0400

    feat: added the other sections

--- scriptscroll.js
+++ scriptscroll.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const offersContainer = document.querySelector(".offers");
+const offersContainer = document.querySelectorAll(".offers");
 const progressBar = document.querySelector(".progress-bar");
 // const leftButton = document.querySelector(".scroll-button.left");
 // const rightButton = document.querySelector(".scroll-button.right");
@@ -8,42 +8,50 @@ let isDown = false;
 let startX;
 let scrollLeft;
 let count = 118;
+console.log(offersContainer);
 
-offersContainer.addEventListener("mousedown", (e) => {
-  isDown = true;
-  startX = e.pageX - offersContainer.offsetLeft;
-  scrollLeft = offersContainer.scrollLeft;
-  console.log(offersContainer.offsetLeft);
+offersContainer.forEach((offersContainer) => {
+  offersContainer.addEventListener("mousedown", (e) => {
+    isDown = true;
+    startX = e.pageX - offersContainer.offsetLeft;
+    scrollLeft = offersContainer.scrollLeft;
+    console.log(offersContainer.offsetLeft);
+  });
 });
 
-offersContainer.addEventListener("mouseleave", () => {
-  isDown = false;
+offersContainer.forEach((offersContainer) => {
+  offersContainer.addEventListener("mouseleave", () => {
+    isDown = false;
+  });
 });
 
-offersContainer.addEventListener("mouseup", () => {
-  isDown = false;
+offersContainer.forEach((offersContainer) => {
+  offersContainer.addEventListener("mouseup", () => {
+    isDown = false;
+  });
 });
-
 let marginAmount;
-offersContainer.addEventListener("mousemove", (e) => {
-  // console.log(e.pageX);
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - offersContainer.offsetLeft;
+offersContainer.forEach((offersContainer) => {
+  offersContainer.addEventListener("mousemove", (e) => {
+    // console.log(e.pageX);
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - offersContainer.offsetLeft;
 
-  const walk = (x - startX) * 2; // Adjust scrolling speed
+    const walk = (x - startX) * 2;
 
-  offersContainer.scrollLeft = scrollLeft - walk;
-  console.log(scrollLeft - walk);
-  if (scrollLeft - walk > 0 && count > 0) {
-    count = count - 3;
-    offersContainer.style.marginLeft = `${count}px`;
-  }
-  if (scrollLeft - walk < 0 && count < 118) {
-    count++;
-    offersContainer.style.marginLeft = `${count}px`;
-  }
-  // updateProgressBar();
+    offersContainer.scrollLeft = scrollLeft - walk;
+    console.log(scrollLeft - walk);
+    if (scrollLeft - walk > 0 && count > 0) {
+      count = count - 3;
+      offersContainer.style.marginLeft = `${count}px`;
+    }
+    if (scrollLeft - walk < 0 && count < 118) {
+      count = count + 4;
+      offersContainer.style.marginLeft = `${count}px`;
+    }
+    updateProgressBar();
+  });
 });
 
 // leftButton.addEventListener("click", () => {
@@ -55,14 +63,17 @@ offersContainer.addEventListener("mousemove", (e) => {
 //   offersContainer.scrollLeft += 200;
 //   updateProgressBar();
 // });
+offersContainer.forEach((offersContainer) => {
+  offersContainer.addEventListener("scroll", updateProgressBar);
+});
 
-// offersContainer.addEventListener("scroll", updateProgressBar);
-
-// function updateProgressBar() {
-//   const containerWidth = document.querySelector(".offers").offsetWidth;
-//   const scrollWidth = offersContainer.scrollWidth - offersContainer.clientWidth;
-//   const scrollLeft = offersContainer.scrollLeft;
-//   const scrollPercentage = (scrollLeft / scrollWidth) * 100;
-//   const progressBarWidth = containerWidth - 100; // Fixed width of the progress bar
-//   progressBar.style.width = (scrollPercentage / 100) * progressBarWidth + "px";
-// }
+function updateProgressBar() {
+  const containerWidth = document.querySelector(".offers").offsetWidth;
+  console.log(document.querySelector(".offers"));
+  const scrollWidth =
+    offersContainer[0].scrollWidth - offersContainer[0].clientWidth;
+  const scrollLeft = offersContainer[0].scrollLeft;
+  const scrollPercentage = (scrollLeft / scrollWidth) * 100;
+  const progressBarWidth = containerWidth - 100; // Fixed width of the progress bar
+  progressBar.style.width = (scrollPercentage / 100) * progressBarWidth + "px";
+}
